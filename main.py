f52b2c1acaad482a1c6164d9f67d77fb44309bb4
celewis <celewis@gmail.com>
2021-10-10 10:37:25 -0400

    Update main.py
    This is the first version distributed to South Florida high schools beginning 29 September 2021

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# GROWNG BEYOND EARTH CONTROL BOX
+# GROWING BEYOND EARTH CONTROL BOX
 # RASPBERRY PI PICO / MICROPYTHON
 
-# FAIRCHILD TROPICAL BOTANIC GARDEN, JULY 9, 2021
+# FAIRCHILD TROPICAL BOTANIC GARDEN, SEPTEMBER 29, 2021
 
 # The Growing Beyond Earth (GBE) control box is a device that controls
 # the LED lights and fan in a GBE growth chamber. It can also control
@@ -10,14 +10,11 @@
 # Micropython.
 
 # This program (main.py) runs automatically each time the device is
-# powered up.  
-
+# powered up.
 
 # -----------------------------------------------------------
 
-# DEFAULT SETTINGS FOR LIGHTS, FAN, AND WATER PUMP
-
-
+# DEFAULT SETTINGS FOR LIGHTS AND FAN
 # LIGHTS -- Time values are hh:mm strings based on a 24h clock;  
 #           brightness values are integers from 0 to 255 that set the
 #           PWM duty cycle
@@ -25,10 +22,10 @@
 lights_on_time = "07:00"
 lights_off_time = "19:00"
 
-red_brightness = 50
-green_brightness = 30
-blue_brightness = 50
-white_brightness = 100
+red_brightness = 72
+green_brightness = 60
+blue_brightness = 44
+white_brightness = 52
 
 
 # FAN -- Fan power is an integer from 0 to 255 that sets the PWM
@@ -37,42 +34,31 @@ white_brightness = 100
 fan_power_when_lights_on = 255
 fan_power_when_lights_off = 128
 
-
-# WATER PUMP -- Sets satering times each day based on a
-#               24h clock as a string in hh:mm format
-
-pump_run_times = ["0:00", "6:00", "12:00", "18:00"]
-soil_moisture_min = 650
-pump_volume_mL = 100
-
-
 # -----------------------------------------------------------
 
 
-loghour=""
+
 
 # Load required libraries from /lib/
 
 from sys import stdin, stdout, exit
 import machine
-import utime
 import time
-import ds3231                       # Real time clock library
-import seesaw, stemma_soil_sensor  # i2C soil moisture sensor library
-import ina219                      # current sensor library
+import utime
+import ds3231              # Hardware real time clock
+import ina219              # Current sensor
+
+
+# Status LED 
+led = machine.PWM(machine.Pin(6))
+led.freq(1000)
 
 
 # Set up I2C bus 0 for devices inside the control box
 
 i2c0 = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17))
-rtc=ds3231.DS3231(i2c0)       # Real time clock
+
 ina = ina219.INA219(0.1,i2c0)   # Current sensor
-
-
-# Set up I2C bus 1 for devices outside the control box
-
-i2c1 = machine.I2C(1, sda=machine.Pin(26), scl=machine.Pin(27), freq=400000)
-seesaw = stemma_soil_sensor.StemmaSoilSensor(i2c1)   # Soil moisture sensor
 
 
 # Set up LED and fan control
@@ -80,56 +66,68 @@ seesaw = stemma_soil_sensor.StemmaSoilSensor(i2c1)   # Soil moisture sensor
 # Set PWM frequency on all channels
 
 r=machine.PWM(machine.Pin(0)); r.freq(20000)   # Red channel
-g=machine.PWM(machine.Pin(2)); g.freq(20000)   # Green channel
-b=machine.PWM(machine.Pin(1)); b.freq(20000)   # Blue channel
+g=machine.PWM(machine.Pin(1)); g.freq(20000)   # Green channel
+b=machine.PWM(machine.Pin(2)); b.freq(20000)   # Blue channel
 w=machine.PWM(machine.Pin(3)); w.freq(20000)   # White channel
 f=machine.PWM(machine.Pin(4)); f.freq(20000)   # Fan
 
+#Clean up lights in case of a crash
+r.duty_u16(0)
+g.duty_u16(0)
+b.duty_u16(0)
+w.duty_u16(0)
 
-# Connect 12v MOSFET to GPIO Pin 6 for on/off control of water pump
 
-pump=machine.Pin(6, machine.Pin.OUT)
+# Set up time Real time clock
+rtc=ds3231.DS3231(i2c0) #read Time from Hardware RTC
+rtc_time_tuple = rtc.DateTime() #Create a string with Time from Hardware RTC
+rtci = machine.RTC()
+rtci.datetime([x for x in rtc_time_tuple] + [0]) #Set local Machine time from Hardware RTC and add a 0 at the end. 
 
-
-# Translate the specified on/off times for lights and water pump
-# to seconds since midnight
+# Translate the specified on/off times to seconds since midnight
 
 onh, onm = map(int, lights_on_time.split(':')); on_seconds = (onh*60 + onm) * 60
 offh, offm = map(int, lights_off_time.split(':')); off_seconds = (offh*60 + offm) * 60
 
-pump_on_seconds = []; pump_off_seconds = []
 
-for t in range(len(pump_run_times)):
-    pumph, pumpm = map(int, pump_run_times[t].split(':'))
-    pump_on_seconds.insert(t, (pumph*60 + pumpm) * 60)
+# Print information at startup
 
-    # Determine how long to run pump based on volume specified.
-    # Peristaltic pump flow rate = 100 mL per minute
+print("\nGROWING BEYOND EARTH, FAIRCHILD TROPICAL BOTANIC GARDEN\n")
 
-    pump_off_seconds.insert(t, (int(pump_on_seconds[t] + ((pump_volume_mL / 100) * 60))))
+print ("Software release date:\n     2021-9-29\n")
 
+print ("Internal clock time:")
+print ("     " + str(rtci.datetime()[0]) + "-" + str(rtci.datetime()[1]) + "-" + str(rtci.datetime()[2]) + "   " + str(rtci.datetime()[4]) + ":" + str(rtci.datetime()[5]) + ":" + str(rtci.datetime()[6]) + "\n") 
+
+# Read from the i2c current sensor
+
+try:
+    ina.configure()
+
+    print("Voltage/current/power sensor readings:")
+    print("     Bus Voltage: %.3f V" % ina.voltage())
+    print("     Current: %.3f mA" % ina.current())
+    print("     Power: %.3f mW" % ina.power())
+    
+except:
+    print("Error reading from the current sensor")
 
 
 # The main loop repeats every second
 
-while True:
-    
-    # Attempt to read the time from the i2c real time clock and
+while True:    
+    # Attempt to read the time from the hardware (i2c) real time clock and
     # catch any errors
       
     try:
-       rtc_dt=rtc.DateTime()
+       rtc_dt=rtci.datetime()
     except:
-      print("An exception has occurred with the RTC")
+      print("An exception has occurred with the internal RTC")
+   
      
     rtc_seconds = ((((rtc_dt[4])*60) + rtc_dt[5]) * 60) + rtc_dt[6]
-     
-
-    #    rtc_dt=utime.localtime()
-    #    rtc_seconds = ((((rtc_dt[3])*60) + rtc_dt[4]) * 60) + rtc_dt[5]
-    
-    
-    # Control lights and fan accoding to schedule
+        
+    # Control lights and fan according to schedule
        
     if rtc_seconds >= on_seconds and rtc_seconds < off_seconds:
         # Lights on
@@ -145,31 +143,9 @@ while True:
         b.duty_u16(int(blue_brightness)*0)
         w.duty_u16(int(white_brightness)*0)
         f.duty_u16(fan_power_when_lights_off * 256)
-    
-    
-    # Control water pump accoding to schedule
-    
-    try:
-        moisture = seesaw.get_moisture()
-    except:
-        moisture = 1000
-        print("An exception has occurred with the moisture sensor")
-    
-    for t in range(len(pump_on_seconds)):
-        if rtc_seconds >= pump_on_seconds[t] and rtc_seconds < pump_off_seconds[t] and moisture < soil_moisture_min:
-             pump.value(1)  # Pump on
-             break
-        else:
-             pump.value(0)  # Pump off
 
-    if loghour != rtc_dt[4]:
-        loghour = rtc_dt[4]
-        logf=open("log.txt", "a")
-        logf.write(str(rtc_dt[4]) + ":" + str(rtc_dt[5]) + ":" + str(rtc_dt[6]) + " Soil Moisture: " + str(moisture) + "\n")
-        logf.close()
-
-    print(str(rtc_dt[4]) + ":" + str(rtc_dt[5]) + ":" + str(rtc_dt[6]) + " Soil Moisture: " + str(moisture) + "\n")
-
-    time.sleep(1) # Wait 1 second before repeating
-    
+    # fade status LED on & off three times
+    for blink in range(3):
+        for duty in range(45000): led.duty_u16(duty); time.sleep(0.0001)
+        for duty in range(45000, 0, -1): led.duty_u16(duty); time.sleep(0.0001)
     
